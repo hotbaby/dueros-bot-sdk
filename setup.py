250ac71ce68b84c59d9fb3290d2da251d8e64001
yy <mengyy_linux@163.com>
2019-01-16 19:05:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import find_packages
 
 setup(
     name='dueros-bot-sdk',
-    version='v1.0.0',
-    author='jack',
-    author_email='mupdf@sina.com',
+    version='v1.0.1',
+    author='Meng.yangyang',
+    author_email='mengyy_linux@163.com',
     description='DuerOS Bot Python SDK',
     keywords='DuerOS Bot SDK',
     license='MIT',
